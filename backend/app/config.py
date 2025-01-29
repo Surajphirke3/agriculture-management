bb9aea9ca39aceea0935a1a8880a4fdad5c7f625
Dhruv Jain <dhruvkishorjain2508@gmail.com>
2025-01-29 21:29:59 +0530

    Atlas Connection Setup

--- backend/app/config.py
+++ backend/app/config.py
@@ -1,6 +1,9 @@
 import os
+from urllib.parse import quote_plus
 
 class Config:
-    MONGO_URI = os.getenv("MONGO_URI", "mongodb://localhost:27017/food_safety")
+    username = quote_plus("dhruvjaink07")
+    password = quote_plus("12345678@abc")
+    MONGO_URI = f"mongodb+srv://{username}:{password}@cluster0.p2x54.mongodb.net/food_safety?retryWrites=true&w=majority"
     DEBUG = True
     SECRET_KEY = os.getenv("SECRET_KEY", "Hello_world")  # Secure session key
