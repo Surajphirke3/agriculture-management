166b6af8d4d311a8a2b09bbd39f0f70a89d85864
Dhruv Jain <dhruvkishorjain2508@gmail.com>
2025-01-29 19:19:19 +0530

    Deploying

--- backend/app/Esp_Api.py
+++ backend/app/Esp_Api.py
@@ -1,13 +1,10 @@
-from flask import Flask, request, jsonify
-from flask_cors import CORS
+from flask import Blueprint, request, jsonify
 
-app = Flask(__name__)
-CORS(app)
-
+esp_api = Blueprint("esp_api", __name__)
 # Store sensor data
 sensor_data = {"temperature": None, "humidity": None}
 
-@app.route("/data", methods=["POST"])
+@esp_api.route("/data", methods=["POST"])
 def receive_data():
     global sensor_data
     data = request.json
@@ -15,9 +12,6 @@ def receive_data():
     sensor_data["humidity"] = data.get("humidity")
     return jsonify({"message": "Data received"}), 200
 
-@app.route("/data", methods=["GET"])
+@esp_api.route("/data", methods=["GET"])
 def send_data():
     return jsonify(sensor_data)
-
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,16 +1,20 @@
 from flask import Flask
+from flask_cors import CORS
+
 from app.extensions import mongo
 from app.config import Config
 from app.inventory.routes.user_routes import auth_bp
 from app.inventory.routes.item_routes import inventory_bp
 # from app.chatbot_routes import assistant_bp
 from app.assistant.chatbot import assistant_bp
+from app.Esp_Api import esp_api
 
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config)
     app.secret_key = app.config["SECRET_KEY"]
+    CORS(app)
 
     # Initialize extension
     mongo.init_app(app)
@@ -19,6 +23,6 @@ def create_app():
     app.register_blueprint(auth_bp,url_prefix="/api/auth")
     app.register_blueprint(inventory_bp, url_prefix="/api/inventory")
     app.register_blueprint(assistant_bp, url_prefix="/api/assistant")
-    
+    app.register_blueprint(esp_api, url_prefix="/api/esp")
     # app.register_blueprint(assistant_bp1, url_prefix="/api/assistant")
     return app
